50e849d15c6c843e939864efedcc69f045b770aa
yamasy1549 <sanae@yamasy.info>
2016-09-07 17:23:27 +0900

    Use zsh

--- scripts/node.sh
+++ scripts/node.sh
@@ -2,6 +2,7 @@ git clone https://github.com/riywo/ndenv.git ~/.ndenv
 mkdir .ndenv/plugins
 git clone https://github.com/riywo/node-build.git ~/.ndenv/plugins/node-build
 
+zsh
 source ~/.zshrc
 
 ndenv install --list
