50a95d3a3395f773bb8739d273a3b60b8e728717
yamasy1549 <sanae@yamasy.info>
2018-02-24 15:37:22 +0900

    $DOTPATHがあった場合はcloneをスキップ

--- setup.sh
+++ setup.sh
@@ -9,24 +9,28 @@ function has() {
     return `type $1 > /dev/null 2>&1`
 }
 
-if has "git"; then
-    git clone "$GITHUB_URL" "$DOTPATH"
-
-elif has "curl" || has "wget"; then
-    tarball="$GITHUB_URL/archive/master.tar.gz"
-
-    if has "curl"; then
-        curl -L "$tarball"
-
-    elif has "wget"; then
-        wget -O - "$tarball"
-
-    fi | tar xv -
-
-    mv -f dotfiles-master "$DOTPATH"
-
+if [ -d $DOTPATH ]; then
+    echo "$DOTPATH already exists."
 else
-    die "curl or wget required"
+    if has "git"; then
+        git clone "$GITHUB_URL" "$DOTPATH"
+
+    elif has "curl" || has "wget"; then
+        tarball="$GITHUB_URL/archive/master.tar.gz"
+
+        if has "curl"; then
+            curl -L "$tarball"
+
+        elif has "wget"; then
+            wget -O - "$tarball"
+
+        fi | tar xv -
+
+        mv -f dotfiles-master "$DOTPATH"
+
+    else
+        die "curl or wget required"
+    fi
 fi
 
 cd $DOTPATH
