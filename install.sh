c69d44d209f14885ea321501d140e1d75c9d6883
yamasy1549 <sanae@yamasy.info>
2018-03-01 13:24:14 +0900

    Ruby, Pythonインストール時にライブラリも自動インストール

--- install.sh
+++ install.sh
@@ -15,10 +15,18 @@ function step() {
     echo "[Step$COUNT]: $*"
 }
 
+DOTPATH=~/dotfiles
+
 
 ##########
 # Rubyのインストール
 ##########
+
+if [ ! -d "$HOME/.rbenv/plugins/rbenv-default-gems" ]; then
+    step "rbenv-default-gems導入"
+    git clone https://github.com/rbenv/rbenv-default-gems.git $HOME/.rbenv/plugins/rbenv-default-gems
+    ln -snfv "$DOTPATH/rbenv-default-gems" "$HOME/.rbenv/default-gems"
+fi
 
 step "最新版のRubyインストール"
 ruby_latest=`rbenv install -l | /usr/bin/grep -v - | tail -1 | tr -d ' '`
@@ -29,6 +37,12 @@ rbenv global $ruby_latest
 ##########
 # Pythonのインストール
 ##########
+
+if [ ! -d "$HOME/.pyenv/plugins/pyenv-default-packages" ]; then
+    step "pyenv-default-packages導入"
+    git clone https://github.com/jawshooah/pyenv-default-packages.git $HOME/.pyenv/plugins/pyenv-default-packages
+    ln -snfv "$DOTPATH/pyenv-default-packages" "$HOME/.pyenv/default-packages"
+fi
 
 step "最新版のPython{2,3}系インストール"
 python2_latest=`pyenv install -l | /usr/bin/grep -e "\s\+2\+.[0-9]\+.[0-9]\+$" | tail -1 | tr -d ' '`
